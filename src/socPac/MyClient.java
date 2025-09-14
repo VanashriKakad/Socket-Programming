f06a9585e46515d03bfc821b2899beed451adefa
VanashriKakad <kakadvanashri@gmail.com>
2025-09-14 17:23:52 +0530

    code for socket programing

---
+++ src/socPac/MyClient.java
@@ -0,0 +1,46 @@
+package socPac;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class MyClient {
+	public static void main(String[] args) {
+		try {
+			 InetAddress ip = InetAddress.getLocalHost(); 
+		        int port = 4444; 
+		        Scanner sc = new Scanner(System.in); 
+		  
+		        // Step 1: Open the socket connection. 
+		        Socket s = new Socket(ip, port); 
+		  
+		        // Step 2: Communication-get the input and output stream 
+		        DataInputStream dis = new DataInputStream(s.getInputStream()); 
+		        DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+		  
+		        while (true) 
+		        { 
+		            // Enter the equation in the form- 
+		            // "operand1 operation operand2" 
+		            System.out.print("Enter the equation in the form: "); 
+		            System.out.println("'operand operator operand'"); 
+		  
+		            String inp = sc.nextLine(); 
+		  
+		            if (inp.equals("bye")) 
+		                break; 
+		  
+		            // send the equation to server 
+		            dos.writeUTF(inp); 
+		  
+		            // wait till request is processed and sent back to client 
+		            String ans = dis.readUTF(); 
+		            System.out.println("Answer=" + ans); 
+		        } 
+		    
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
